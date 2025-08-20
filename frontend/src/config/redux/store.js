02f46ed09c1686a66ed55b06fe696160898e0082
Satyam Jha <satyamjha2743@gmil.com>
2025-08-20 20:33:50 +0530

    Boilerplate: boilerplate code for redux

---
+++ frontend/src/config/redux/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+/**
+ * 
+ *STEPS for state management
+
+ 1. Submit action
+ 2. handle action in it's Reducer
+ 3. Register here -> Reducer
+ */
+
+export const store = configureStore({
+  reducer: {},
+});
