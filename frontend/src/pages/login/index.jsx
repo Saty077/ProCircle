f0411e0179954b509445974abcc9731f3da6ccb0
Satyam Jha <satyamjha2743@gmil.com>
2025-08-28 19:05:05 +0530

    feat: creating the authentication component UI

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,11 +1,65 @@
 import UserLayout from "@/layout/userLayout";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import styles from "./style.module.css";
 
 function LoginComponent() {
+  const authState = useSelector((state) => state.auth);
+  const router = useRouter();
+  const [loginMethod, setLoginMethod] = useState(false);
+  useEffect(() => {
+    if (authState.loggedIn) {
+      router.push("/dashboard");
+    }
+  }, [authState.loggedIn, router]);
+
   return (
-    <div>
-      <UserLayout>LoginComponent</UserLayout>
-    </div>
+    <UserLayout>
+      <div className={styles.container}>
+        <div className={styles.cardContainer}>
+          <div className={styles.cardContainer_left}>
+            <p className={styles.cardLeft_heading}>
+              {loginMethod ? "Sign In" : "Sign Up"}
+            </p>
+
+            <div className={styles.cardInputs}>
+              <div className={styles.inputRows}>
+                <input
+                  className={styles.inputFields}
+                  type="text"
+                  placeholder="Username"
+                />
+                <input
+                  className={styles.inputFields}
+                  type="text"
+                  placeholder="Name"
+                />
+              </div>
+              <input
+                className={styles.inputFields}
+                type="text"
+                placeholder="email"
+              />
+              <input
+                className={styles.inputFields}
+                type="text"
+                placeholder="password"
+              />
+              <div
+                onClick={() => {
+                  console.log("Registered!!!!!!!!!!!!");
+                }}
+                className={styles.submitBtn}
+              >
+                Sign Up
+              </div>
+            </div>
+          </div>
+          <div className={styles.cardContainer_right}></div>
+        </div>
+      </div>
+    </UserLayout>
   );
 }
 
